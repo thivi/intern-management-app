7bc6bc604f9786d11bdc70cebde95d62f8ee8fe6
Thivi <theviyanthan@outlook.com>
2020-05-23 02:06:22 +0530

    Modify sign in to accommodate gapi oauth

--- src/utils/sign-in.ts
+++ src/utils/sign-in.ts
@@ -1,13 +1,12 @@
 import { AuthAction } from "../models";
-import { SIGN_IN, ACCESS_TOKEN, CALLBACK_URL } from "../constants";
+import { SIGN_IN, CALLBACK_URL } from "../constants";
 
 export const signIn = (dispatch: React.Dispatch<AuthAction>, accessToken: string) => {
-	localStorage.setItem(ACCESS_TOKEN, accessToken);
 	dispatch({ type: SIGN_IN, payload: accessToken });
 };
 
 export const getAccessToken = ():string => {
-	return localStorage.getItem(ACCESS_TOKEN) ?? "";
+	return gapi.client.getToken().access_token;
 };
 
 export const updateCallbackUrl = (url: string) => {
