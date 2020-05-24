36de93bb8b4cb1d1fd6aff129efaa432e76d7462
Thivi <theviyanthan@outlook.com>
2020-05-25 00:19:03 +0530

    Add git issues model

---
+++ src/models/git-issues.ts
@@ -0,0 +1,6 @@
+export interface GitIssue{
+    Email_ID: string;
+    Issue_Title: string;
+    Link: string;
+    id?: string;
+}
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,3 +1,4 @@
 export * from "./authentication";
 export * from "./routes";
 export * from "./profile";
+export * from "./git-issues";
