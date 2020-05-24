5135a66b55dc4f81be845c8e54ea2a3ac6632969
Thivi <theviyanthan@outlook.com>
2020-05-25 01:10:48 +0530

    Add pull requests to routes constant

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -3,3 +3,4 @@ export const LOGIN = "/login";
 export const LOGOUT = "/logout";
 export const PROFILE = "/profile";
 export const GIT_ISSUES_PATH = "/git-issues";
+export const PULL_REQUESTS_PATH = "/pull-requests";
