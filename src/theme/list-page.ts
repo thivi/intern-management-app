d7d436c6d47724dd2f72b4e982014f79f97ddc27
Thivi <theviyanthan@outlook.com>
2020-05-25 00:18:45 +0530

    Add list page theme classes

--- src/theme/list-page.ts
+++ src/theme/list-page.ts
@@ -1,21 +1,15 @@
 import { Theme } from "@material-ui/core";
 
-export const gridGeneric = (theme: Theme) => ({
-    padding: theme.spacing(3)
+export const gridForm = (theme: Theme) => ({
+	width: "100%",
+	display: "flex",
 });
 
-export const listSecondary = (theme: Theme) => ({
+export const gridRightMargin = (theme: Theme) => ({
+    marginRight: theme.spacing(1)
+})
+
+export const pagination = (theme: Theme) => ({
     display: "flex",
-    flexDirection: "column" as const
-});
-
-export const dangerButton = (theme: Theme) => ({
-    color: "red"
-});
-
-export const fab = (theme: Theme) => ({
-    position: "fixed" as const,
-    zIndex: 100,
-    top: "calc(100vh - 175px)",
-    right: "100px"
+    justifyContent:"center"
 })
