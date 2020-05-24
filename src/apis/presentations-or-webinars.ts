606ded620e2fdbb5b8f9d4f66e75750573c8ea47
Thivi <theviyanthan@outlook.com>
2020-05-25 01:31:21 +0530

    Add presentations or webinars apis

---
+++ src/apis/presentations-or-webinars.ts
@@ -0,0 +1,96 @@
+import { Methods, http } from "../configs";
+import { AxiosRequestConfig } from "axios";
+import { apiEndpoints, PRESENTATIONS_OR_WEBINARS } from "../constants";
+import { errorStatus } from "../utils";
+
+export const getPresentationsOrWebinars = (): Promise<any> => {
+	const config: AxiosRequestConfig = {
+		method: Methods.GET,
+		url: `${apiEndpoints.sheet}/${PRESENTATIONS_OR_WEBINARS}`,
+	};
+
+	return http(config)
+		.then((response) => {
+			if (response.status !== 200) {
+				return Promise.reject(errorStatus(response.status));
+			}
+
+			return Promise.resolve(response.data);
+		})
+		.catch((error) => {
+			return Promise.reject(error?.response?.data);
+		});
+};
+
+export const updatePresentationsOrWebinars = (range: string, values: string[]): Promise<any> => {
+	const config: AxiosRequestConfig = {
+		method: Methods.PUT,
+		url: `${apiEndpoints.sheet}/${range}`,
+		data: {
+			range,
+			majorDimension: "ROWS",
+			values: [values],
+		},
+		params: {
+			valueInputOption: "USER_ENTERED",
+		},
+	};
+
+	return http(config)
+		.then((response) => {
+			if (response.status !== 200) {
+				return Promise.reject(errorStatus(response.status));
+			}
+
+			return Promise.resolve(response.data);
+		})
+		.catch((error) => {
+			return Promise.reject(error?.response?.data);
+		});
+};
+
+export const addPresentationsOrWebinars = (values: string[]): Promise<any> => {
+	const config: AxiosRequestConfig = {
+		method: Methods.POST,
+		url: `${apiEndpoints.sheet}/${PRESENTATIONS_OR_WEBINARS}:append`,
+		data: {
+			range: PRESENTATIONS_OR_WEBINARS,
+			majorDimension: "ROWS",
+			values: [values],
+		},
+		params: {
+			valueInputOption: "USER_ENTERED",
+		},
+	};
+
+	return http(config)
+		.then((response) => {
+			if (response.status !== 200) {
+				return Promise.reject(errorStatus(response.status));
+			}
+
+			return Promise.resolve(response.data);
+		})
+		.catch((error) => {
+			return Promise.reject(error?.response?.data);
+		});
+};
+
+export const deletePresentationOrWebinar = (range: string): Promise<any> => {
+	const config: AxiosRequestConfig = {
+		method: Methods.POST,
+		url: `${apiEndpoints.sheet}/${range}:clear`
+	};
+
+	return http(config)
+		.then((response) => {
+			if (response.status !== 200) {
+				return Promise.reject(errorStatus(response.status));
+			}
+
+			return Promise.resolve(response.data);
+		})
+		.catch((error) => {
+			return Promise.reject(error?.response?.data);
+		});
+};
