4bcfd5b1feae2b29e530162a23d487e5ed74a86d
Thivi <theviyanthan@outlook.com>
2020-05-25 01:52:23 +0530

    Add blogs sheet constant

--- src/constants/sheets.ts
+++ src/constants/sheets.ts
@@ -2,3 +2,4 @@ export const INTERN_PROFILE = "Intern_Profile";
 export const GIT_ISSUES = "Git_Issues";
 export const PULL_REQUESTS = "Pull_Requests";
 export const PRESENTATIONS_OR_WEBINARS = "Presentations_Or_Webinars";
+export const BLOGS = "Blogs";
