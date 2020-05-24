0481242860c12b4ccc264ce038bdfba6a4fbc8d3
Thivi <theviyanthan@outlook.com>
2020-05-25 01:52:16 +0530

    Add blogs route constant

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -5,3 +5,4 @@ export const PROFILE = "/profile";
 export const GIT_ISSUES_PATH = "/git-issues";
 export const PULL_REQUESTS_PATH = "/pull-requests";
 export const PRESENTATIONS_OR_WEBINARS_PATH = "/presentations-or-webinars";
+export const BLOGS_PATH = "/blogs";
