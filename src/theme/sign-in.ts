add20da3736de5bde3e003c81e751f10c68340be
Thivi <theviyanthan@outlook.com>
2020-05-25 00:16:03 +0530

    Add sign in theme

--- src/theme/sign-in.ts
+++
@@ -1,9 +0,0 @@
-import { Theme } from "@material-ui/core";
-
-export const signInGrid = (theme: Theme) => ({
-	minHeight: "100vh"
-});
-
-export const signInPaper = (theme: Theme) => ({
-	padding: theme.spacing(2)
-});
