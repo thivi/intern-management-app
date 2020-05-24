6322aba83466815d7c36f067961ea0386485f589
Thivi <theviyanthan@outlook.com>
2020-05-25 01:29:49 +0530

    Add presentations or webinars constants

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -4,3 +4,4 @@ export const LOGOUT = "/logout";
 export const PROFILE = "/profile";
 export const GIT_ISSUES_PATH = "/git-issues";
 export const PULL_REQUESTS_PATH = "/pull-requests";
+export const PRESENTATIONS_OR_WEBINARS_PATH = "/presentations-or-webinars";
--- src/constants/sheets.ts
+++ src/constants/sheets.ts
@@ -1,3 +1,4 @@
 export const INTERN_PROFILE = "Intern_Profile";
 export const GIT_ISSUES = "Git_Issues";
 export const PULL_REQUESTS = "Pull_Requests";
+export const PRESENTATIONS_OR_WEBINARS = "Presentations_Or_Webinars";
