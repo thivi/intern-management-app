27412119e82da35b91c3a28d7edf0b2b23a194ba
Thivi <theviyanthan@outlook.com>
2020-05-31 02:45:25 +0530

    Add roles api

---
+++ src/apis/roles.ts
@@ -0,0 +1,96 @@
+import { Methods, http } from "../configs";
+import { AxiosRequestConfig } from "axios";
+import { apiEndpoints, ROLES } from "../constants";
+import { errorStatus } from "../utils";
+
+export const getRoles = (): Promise<any> => {
+	const config: AxiosRequestConfig = {
+		method: Methods.GET,
+		url: `${apiEndpoints.sheet}/${ROLES}`,
+	};
+
+	return http(config)
+		.then((response) => {
+			if (response.status !== 200) {
+				return Promise.reject(errorStatus(response.status));
+			}
+
+			return Promise.resolve(response.data);
+		})
+		.catch((error) => {
+			return Promise.reject(error?.response?.data);
+		});
+};
+
+export const updateRoles = (range: string, values: string[]): Promise<any> => {
+	const config: AxiosRequestConfig = {
+		method: Methods.PUT,
+		url: `${apiEndpoints.sheet}/${range}`,
+		data: {
+			range,
+			majorDimension: "ROWS",
+			values: [values],
+		},
+		params: {
+			valueInputOption: "USER_ENTERED",
+		},
+	};
+
+	return http(config)
+		.then((response) => {
+			if (response.status !== 200) {
+				return Promise.reject(errorStatus(response.status));
+			}
+
+			return Promise.resolve(response.data);
+		})
+		.catch((error) => {
+			return Promise.reject(error?.response?.data);
+		});
+};
+
+export const addRoles = (values: string[]): Promise<any> => {
+	const config: AxiosRequestConfig = {
+		method: Methods.POST,
+		url: `${apiEndpoints.sheet}/${ROLES}:append`,
+		data: {
+			range: ROLES,
+			majorDimension: "ROWS",
+			values: [values],
+		},
+		params: {
+			valueInputOption: "USER_ENTERED",
+		},
+	};
+
+	return http(config)
+		.then((response) => {
+			if (response.status !== 200) {
+				return Promise.reject(errorStatus(response.status));
+			}
+
+			return Promise.resolve(response.data);
+		})
+		.catch((error) => {
+			return Promise.reject(error?.response?.data);
+		});
+};
+
+export const deleteRole = (range: string): Promise<any> => {
+	const config: AxiosRequestConfig = {
+		method: Methods.POST,
+		url: `${apiEndpoints.sheet}/${range}:clear`
+	};
+
+	return http(config)
+		.then((response) => {
+			if (response.status !== 200) {
+				return Promise.reject(errorStatus(response.status));
+			}
+
+			return Promise.resolve(response.data);
+		})
+		.catch((error) => {
+			return Promise.reject(error?.response?.data);
+		});
+};
