40fe3918ba0d510c133b562836adc3bd0137a7a8
Thivi <theviyanthan@outlook.com>
2020-05-31 03:07:26 +0530

    Update authentication model

--- src/models/authentication.ts
+++ src/models/authentication.ts
@@ -1,29 +1,35 @@
 import { SIGN_IN, ADD_DETAILS, SIGN_OUT } from "./../constants/authentication";
 import { GoogleProfile } from "./profile";
+import { RoleType } from ".";
+
+interface AuthData extends GoogleProfile {
+	role: RoleType;
+}
 
 export interface AuthState {
 	authenticated: boolean;
-    accessToken: string | null;
-    authData: GoogleProfile;
+	accessToken: string | null;
+	authData: AuthData;
 }
 
 export interface SignIn {
-    type: typeof SIGN_IN;
-    payload: string;
+	type: typeof SIGN_IN;
+	payload: string;
 }
 
 export interface SignOut {
-    type: typeof SIGN_OUT;
-    payload: null;
+	type: typeof SIGN_OUT;
+	payload: null;
 }
 
 export interface AddDetails {
-    type: typeof ADD_DETAILS;
-    payload: GoogleProfile;
+	type: typeof ADD_DETAILS;
+	payload: GoogleProfile | { role: RoleType };
 }
+
 export type AuthAction = AddDetails | SignIn | SignOut;
 
 export interface AuthContextInterface {
-    authState: AuthState;
-    dispatch: React.Dispatch<AuthAction>;
+	authState: AuthState;
+	dispatch: React.Dispatch<AuthAction>;
 }
