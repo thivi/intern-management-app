6f33997e6a3e8f33dac0d314dcb5a34c6782a596
Thivi <theviyanthan@outlook.com>
2020-06-05 23:59:53 +0530

    Implement profiles tab

--- src/pages/intern-profile-tabs/profile.tsx
+++ src/pages/intern-profile-tabs/profile.tsx
@@ -1,5 +1,68 @@
 import React, { ReactElement } from "react";
+import { Profile } from "../../models";
+import { Grid, Typography } from "@material-ui/core";
 
-export const ProfileTab = (): ReactElement => {
-	return <div>Profile</div>;
+interface ProfileTabPropsInterface {
+	profile: Profile;
+}
+
+export const ProfileTab = (props: ProfileTabPropsInterface): ReactElement => {
+	const { profile } = props;
+	return (
+		<Grid container spacing={2}>
+			<Typography variant="h6">Personal Information</Typography>
+			<Grid item xs={12}>
+				<Typography>Name</Typography>
+				<Typography>{profile.Name}</Typography>
+			</Grid>
+			<Grid item xs={12}>
+				<Typography>Email</Typography>
+				<Typography>{profile.Email_ID}</Typography>
+			</Grid>
+			<Typography variant="h6">Academic Credentials</Typography>
+			<Grid item xs={12}>
+				<Typography>University</Typography>
+				<Typography>{profile.University}</Typography>
+			</Grid>
+			<Grid item xs={12}>
+				<Typography>Degree</Typography>
+				<Typography>{profile.Degree}</Typography>
+			</Grid>
+
+			<Typography variant="h6">Internship Period</Typography>
+			<Grid item xs={12}>
+				<Typography>Joined Date</Typography>
+				<Typography>{profile.Joined_date}</Typography>
+			</Grid>
+			<Grid item xs={12}>
+				<Typography>Leaving Date</Typography>
+				<Typography>{profile.Leaving_date}</Typography>
+			</Grid>
+
+			<Typography variant="h6">Contacts</Typography>
+			<Grid item xs={12}>
+				<Typography>Contact No.</Typography>
+				<Typography>{profile.Contact_no}</Typography>
+			</Grid>
+
+			<Typography variant="h6">Mentors</Typography>
+			<Grid item xs={12}>
+				<Typography>Mentor</Typography>
+				<Typography>{profile.Mentor}</Typography>
+			</Grid>
+			<Grid item xs={12}>
+				<Typography>Co-mentor</Typography>
+				<Typography>{profile.Co_mentor}</Typography>
+			</Grid>
+			<Typography variant="h6">Links</Typography>
+			<Grid item xs={12}>
+				<Typography>Blog</Typography>
+				<Typography>{profile.Blog}</Typography>
+			</Grid>
+			<Grid item xs={12}>
+				<Typography>Gantt Chart</Typography>
+				<Typography>{profile.Gantt_chart}</Typography>
+			</Grid>
+		</Grid>
+	);
 };
