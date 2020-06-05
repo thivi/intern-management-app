d3b79c23916f44c497649d02696709a3877c8dd7
Thivi <theviyanthan@outlook.com>
2020-06-05 23:59:39 +0530

    Index tabs

--- src/pages/intern-profile-tabs/index.ts
+++ src/pages/intern-profile-tabs/index.ts
@@ -1,1 +1,7 @@
 export * from "./profile";
+export * from "./git-issues-tab";
+export * from "./blogs-tab";
+export * from "./presentations-or-webinars-tab";
+export * from "./project-tasks-tab";
+export * from "./projects-tab";
+export * from "./pull-requests-tab";
