bb5e147bd66f10989953a2da45107f65caa76a7a
Thivi <theviyanthan@outlook.com>
2020-06-06 23:42:09 +0530

    Remove auto redirect

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-import { BrowserRouter, Switch, Redirect, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { Authentication } from "./helpers/authentication";
 import { routes } from "./configs";
 import { ProtectedRoute } from "./helpers";
 import { RouteInterface } from "./models/routes";
-import { HOME } from "./constants";
 import { AppLayout } from "./layout";
 
 export const App = (): React.ReactElement => {
@@ -12,7 +11,6 @@ export const App = (): React.ReactElement => {
 		<BrowserRouter>
 			<Authentication>
 				<Switch>
-					<Redirect exact={true} path="/" to={HOME} />
 					{routes?.map((route: RouteInterface, index: number) => {
 						if (route.protected) {
 							return (
