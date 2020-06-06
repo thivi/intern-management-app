713ffa171a654fae4177ee014870e27ab769b40b
Thivi <theviyanthan@outlook.com>
2020-06-06 21:40:29 +0530

    Add Dashboard page

--- src/pages/dashboard/dashboard.tsx
+++ src/pages/dashboard/dashboard.tsx
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AuthContext } from "../../helpers";
+import { DashboardIntern } from "./dashboard-intern";
+import { DashboardMentor } from "./dashboard-mentor";
+import { Redirect } from "react-router-dom";
 
 export const Dashboard = (): React.ReactElement => {
+	const { authState } = useContext(AuthContext);
 
-	return <div>Dashboard</div>;
+	switch (authState.authData.role) {
+		case "intern":
+			return <DashboardIntern />;
+		case "mentor":
+			return <DashboardMentor />;
+		default:
+			return <Redirect to="/" />;
+	}
 };
