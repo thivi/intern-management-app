714e6b9bd9dd313e8c7622ba2b3ad7b77fa8fe0d
Thivi <theviyanthan@outlook.com>
2020-06-08 04:05:06 +0530

    Add theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -5,44 +5,48 @@ import { routes } from "./configs";
 import { ProtectedRoute } from "./helpers";
 import { RouteInterface } from "./models/routes";
 import { AppLayout } from "./layout";
+import { ThemeProvider } from "@material-ui/core";
+import { theme } from "./theme";
 
 export const App = (): React.ReactElement => {
 	return (
 		<BrowserRouter>
 			<Authentication>
-				<Switch>
-					{routes?.map((route: RouteInterface, index: number) => {
-						if (route.protected) {
-							return (
-								<ProtectedRoute
-									key={index}
-									path={route.path}
-									exact={route.exact}
-									component={route.component}
-									appLayout={route.appLayout}
-									permission={route.permission}
-								/>
-							);
-						} else {
-							return (
-								<Route
-									key={index}
-									path={route.path}
-									exact={route.exact}
-									component={
-										route.appLayout
-											? () => (
-													<AppLayout>
-														<route.component />
-													</AppLayout>
-											  )
-											: route.component
-									}
-								/>
-							);
-						}
-					})}
-				</Switch>
+				<ThemeProvider theme={theme}>
+					<Switch>
+						{routes?.map((route: RouteInterface, index: number) => {
+							if (route.protected) {
+								return (
+									<ProtectedRoute
+										key={index}
+										path={route.path}
+										exact={route.exact}
+										component={route.component}
+										appLayout={route.appLayout}
+										permission={route.permission}
+									/>
+								);
+							} else {
+								return (
+									<Route
+										key={index}
+										path={route.path}
+										exact={route.exact}
+										component={
+											route.appLayout
+												? () => (
+														<AppLayout>
+															<route.component />
+														</AppLayout>
+												  )
+												: route.component
+										}
+									/>
+								);
+							}
+						})}
+					</Switch>
+				</ThemeProvider>
 			</Authentication>
 		</BrowserRouter>
 	);
