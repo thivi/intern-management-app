9b63247872d3e0cfaafbd54ab3155d68b568bf99
Thivi <theviyanthan@outlook.com>
2020-06-08 04:06:57 +0530

    Style profiles page

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,7 +1,7 @@
 import React, { ReactElement, useEffect, useState, useCallback, useContext } from "react";
 import { getProfile, updateProfile, addProfile } from "../apis";
 import { Profile } from "../models";
-import { Grid, Avatar, Typography, TextField, Button } from "@material-ui/core";
+import { Grid, Avatar, Typography, TextField, Button, Paper } from "@material-ui/core";
 import { useFormik } from "formik";
 import { convertKeyToLabel } from "../utils";
 import { MuiPickersUtilsProvider, KeyboardDatePicker } from "@material-ui/pickers";
@@ -11,6 +11,7 @@ import { Skeleton } from "@material-ui/lab";
 import validator from "validator";
 import { AuthContext } from "../helpers";
 import { INTERN_PROFILE } from "../constants";
+import useStyles from "../theme";
 
 export const ProfilePage = (): ReactElement => {
 	const [profile, setProfile] = useState<Profile>(null);
@@ -18,6 +19,8 @@ export const ProfilePage = (): ReactElement => {
 	const [range, setRange] = useState("");
 
 	const { authState } = useContext(AuthContext);
+
+	const classes = useStyles();
 
 	const formik = useFormik({
 		onSubmit: (values, { setSubmitting }) => {
@@ -146,205 +149,246 @@ export const ProfilePage = (): ReactElement => {
 	return (
 		<>
 			<Grid container spacing={2}>
-				<Grid item xs={4}>
-					<Avatar src={authState.authData?.picture} />
+				<Grid item xs={2}>
+					<Avatar className={classes.profileAvatar} src={authState.authData?.picture} />
 				</Grid>
-				<Grid item xs={8}>
-					<Typography variant="h5">{authState.authData?.name}</Typography>
-					<Typography>{authState.authData?.email}</Typography>
+				<Grid item xs={10}>
+					<Typography variant="h4">{authState.authData?.name}</Typography>
+					<Typography variant="h5" color="textSecondary">
+						{authState.authData?.email}
+					</Typography>
 				</Grid>
 			</Grid>
 
 			<form noValidate onSubmit={handleSubmit}>
+				<Paper className={classes.fieldsPaper}>
+					<Typography variant="h5" className={classes.fieldsHeader}>
+						Academic Credentials
+					</Typography>
+					<Grid container spacing={2}>
+						<Grid item xs={12}>
+							{isLoading ? (
+								<Skeleton variant="text" height={80} />
+							) : (
+								<TextField
+									name="university"
+									placeholder="eg: University of Colombo, University of Moratuwa etc."
+									value={values.university ?? ""}
+									onChange={handleChange}
+									variant="outlined"
+									label="University"
+									fullWidth
+									helperText={touched.university && errors.university && errors.university}
+									onBlur={handleBlur}
+									error={!!(touched.university && errors.university)}
+								/>
+							)}
+						</Grid>
+						<Grid item xs={12}>
+							{isLoading ? (
+								<Skeleton variant="text" height={80} />
+							) : (
+								<TextField
+									name="degree"
+									placeholder={
+										"eg: B.Sc. in Information Technology, B.Eng. in Software Engineering etc."
+									}
+									value={values.degree ?? ""}
+									variant="outlined"
+									label="Degree"
+									fullWidth
+									onChange={handleChange}
+									helperText={touched.degree && errors.degree && errors.degree}
+									onBlur={handleBlur}
+									error={!!(touched.degree && errors.degree)}
+								/>
+							)}
+						</Grid>
+					</Grid>
+				</Paper>
+				<Paper className={classes.fieldsPaper}>
+					<Typography variant="h5" className={classes.fieldsHeader}>
+						Internship Period
+					</Typography>
+					<Grid container spacing={2}>
+						<Grid item xs={12}>
+							<MuiPickersUtilsProvider utils={MomentUtils}>
+								{isLoading ? (
+									<Skeleton variant="text" height={80} />
+								) : (
+									<KeyboardDatePicker
+										name="joined_date"
+										placeholder={"eg: " + new Date().toDateString()}
+										value={values.joined_date}
+										label="Joined Date"
+										fullWidth
+										helperText={touched.joined_date && errors.joined_date && errors.joined_date}
+										onBlur={handleBlur}
+										margin="normal"
+										format="DD-MM-YYYY"
+										onChange={(date: Moment) => {
+											setFieldValue("joined_date", date);
+										}}
+										KeyboardButtonProps={{
+											"aria-label": "change date",
+										}}
+										error={!!(touched.joined_date && errors.joined_date)}
+									/>
+								)}
+							</MuiPickersUtilsProvider>
+						</Grid>
+						<Grid item xs={12}>
+							<MuiPickersUtilsProvider utils={MomentUtils}>
+								{isLoading ? (
+									<Skeleton variant="text" height={80} />
+								) : (
+									<KeyboardDatePicker
+										name="leaving_date"
+										placeholder={
+											"eg: " + new Date(new Date().getTime() + 15552000000).toDateString()
+										}
+										label="Leaving Date"
+										fullWidth
+										onBlur={handleBlur}
+										margin="normal"
+										format="DD-MM-YYYY"
+										onChange={(date: Moment) => {
+											setFieldValue("leaving_date", date);
+										}}
+										KeyboardButtonProps={{
+											"aria-label": "change date",
+										}}
+										value={values.leaving_date}
+										helperText={touched.leaving_date && errors.leaving_date && errors.leaving_date}
+										error={!!(touched.leaving_date && errors.leaving_date)}
+									/>
+								)}
+							</MuiPickersUtilsProvider>
+						</Grid>
+					</Grid>
+				</Paper>
+
+				<Paper className={classes.fieldsPaper}>
+					<Typography variant="h5" className={classes.fieldsHeader}>
+						Contacts
+					</Typography>
+					<Grid container spacing={2}>
+						<Grid item xs={12}>
+							{isLoading ? (
+								<Skeleton variant="text" height={80} />
+							) : (
+								<TextField
+									name="contact_number"
+									placeholder="eg: 077756896532"
+									value={values.contact_number ?? ""}
+									variant="outlined"
+									label="Contact Number"
+									fullWidth
+									onChange={handleChange}
+									helperText={
+										touched.contact_number && errors.contact_number && errors.contact_number
+									}
+									onBlur={handleBlur}
+									error={!!(touched.contact_number && errors.contact_number)}
+								/>
+							)}
+						</Grid>
+					</Grid>
+				</Paper>
+
+				<Paper className={classes.fieldsPaper}>
+					<Typography variant="h5" className={classes.fieldsHeader}>
+						Mentors
+					</Typography>
+					<Grid container spacing={2}>
+						<Grid item xs={12}>
+							{isLoading ? (
+								<Skeleton variant="text" height={80} />
+							) : (
+								<TextField
+									name="mentor"
+									placeholder="eg: John Doe"
+									value={values.mentor ?? ""}
+									variant="outlined"
+									label="Mentor email"
+									fullWidth
+									onChange={handleChange}
+									helperText={touched.mentor && errors.mentor && errors.mentor}
+									onBlur={handleBlur}
+									error={!!(touched.mentor && errors.mentor)}
+								/>
+							)}
+						</Grid>
+						<Grid item xs={12}>
+							{isLoading ? (
+								<Skeleton variant="text" height={80} />
+							) : (
+								<TextField
+									name="co_mentor"
+									placeholder="eg: John Doe"
+									value={values.co_mentor ?? ""}
+									variant="outlined"
+									label="Co-mentor email"
+									fullWidth
+									onChange={handleChange}
+									helperText={touched.co_mentor && errors.co_mentor && errors.co_mentor}
+									onBlur={handleBlur}
+									error={!!(touched.co_mentor && errors.co_mentor)}
+								/>
+							)}
+						</Grid>
+					</Grid>
+				</Paper>
+
+				<Paper className={classes.fieldsPaper}>
+					<Typography variant="h5" className={classes.fieldsHeader}>
+						Links
+					</Typography>
+					<Grid container spacing={2}>
+						<Grid item xs={12}>
+							{isLoading ? (
+								<Skeleton variant="text" height={80} />
+							) : (
+								<TextField
+									name="blog"
+									placeholder="eg: https://www.thearmchaircritic.org"
+									value={values.blog ?? ""}
+									variant="outlined"
+									label="Blog Link"
+									fullWidth
+									onChange={handleChange}
+									helperText={touched.blog && errors.blog && errors.blog}
+									onBlur={handleBlur}
+									error={!!(touched.blog && errors.blog)}
+								/>
+							)}
+						</Grid>
+						<Grid item xs={12}>
+							{isLoading ? (
+								<Skeleton variant="text" height={80} />
+							) : (
+								<TextField
+									name="gantt_chart"
+									placeholder="eg: https://docs.google.com/..."
+									value={values.gantt_chart ?? ""}
+									variant="outlined"
+									label="Link to Gantt Chart"
+									fullWidth
+									onChange={handleChange}
+									helperText={touched.gantt_chart && errors.gantt_chart && errors.gantt_chart}
+									onBlur={handleBlur}
+									error={!!(touched.gantt_chart && errors.gantt_chart)}
+								/>
+							)}
+						</Grid>
+					</Grid>
+				</Paper>
 				<Grid container spacing={2}>
-					<Typography variant="h6">Academic Credentials</Typography>
-					<Grid item xs={12}>
-						{isLoading ? (
-							<Skeleton variant="text" />
-						) : (
-							<TextField
-								name="university"
-								placeholder="eg: University of Colombo, University of Moratuwa etc."
-								value={values.university ?? ""}
-								onChange={handleChange}
-								variant="outlined"
-								label="University"
-								fullWidth
-								helperText={touched.university && errors.university && errors.university}
-								onBlur={handleBlur}
-								error={!!(touched.university && errors.university)}
-							/>
-						)}
+					<Grid item xs={12} className={classes.leftAlignedGrid}>
+						<Button className={classes.primaryButton} color="primary" variant="contained" type="submit">
+							Submit
+						</Button>
 					</Grid>
-					<Grid item xs={12}>
-						{isLoading ? (
-							<Skeleton variant="text" />
-						) : (
-							<TextField
-								name="degree"
-								placeholder={"eg: B.Sc. in Information Technology, B.Eng. in Software Engineering etc."}
-								value={values.degree ?? ""}
-								variant="outlined"
-								label="Degree"
-								fullWidth
-								onChange={handleChange}
-								helperText={touched.degree && errors.degree && errors.degree}
-								onBlur={handleBlur}
-								error={!!(touched.degree && errors.degree)}
-							/>
-						)}
-					</Grid>
-
-					<Typography variant="h6">Internship Period</Typography>
-					<Grid item xs={12}>
-						<MuiPickersUtilsProvider utils={MomentUtils}>
-							{isLoading ? (
-								<Skeleton variant="text" />
-							) : (
-								<KeyboardDatePicker
-									name="joined_date"
-									placeholder={"eg: " + new Date().toDateString()}
-									value={values.joined_date}
-									label="Joined Date"
-									fullWidth
-									helperText={touched.joined_date && errors.joined_date && errors.joined_date}
-									onBlur={handleBlur}
-									margin="normal"
-									format="DD-MM-YYYY"
-									onChange={(date: Moment) => {
-										setFieldValue("joined_date", date);
-									}}
-									KeyboardButtonProps={{
-										"aria-label": "change date",
-									}}
-									error={!!(touched.joined_date && errors.joined_date)}
-								/>
-							)}
-						</MuiPickersUtilsProvider>
-					</Grid>
-					<Grid item xs={12}>
-						<MuiPickersUtilsProvider utils={MomentUtils}>
-							{isLoading ? (
-								<Skeleton variant="text" />
-							) : (
-								<KeyboardDatePicker
-									name="leaving_date"
-									placeholder={"eg: " + new Date(new Date().getTime() + 15552000000).toDateString()}
-									label="Leaving Date"
-									fullWidth
-									onBlur={handleBlur}
-									margin="normal"
-									format="DD-MM-YYYY"
-									onChange={(date: Moment) => {
-										setFieldValue("leaving_date", date);
-									}}
-									KeyboardButtonProps={{
-										"aria-label": "change date",
-									}}
-									value={values.leaving_date}
-									helperText={touched.leaving_date && errors.leaving_date && errors.leaving_date}
-									error={!!(touched.leaving_date && errors.leaving_date)}
-								/>
-							)}
-						</MuiPickersUtilsProvider>
-					</Grid>
-
-					<Typography variant="h6">Contacts</Typography>
-					<Grid item xs={12}>
-						{isLoading ? (
-							<Skeleton variant="text" />
-						) : (
-							<TextField
-								name="contact_number"
-								placeholder="eg: 077756896532"
-								value={values.contact_number ?? ""}
-								variant="outlined"
-								label="Contact Number"
-								fullWidth
-								onChange={handleChange}
-								helperText={touched.contact_number && errors.contact_number && errors.contact_number}
-								onBlur={handleBlur}
-								error={!!(touched.contact_number && errors.contact_number)}
-							/>
-						)}
-					</Grid>
-
-					<Typography variant="h6">Mentors</Typography>
-					<Grid item xs={12}>
-						{isLoading ? (
-							<Skeleton variant="text" />
-						) : (
-							<TextField
-								name="mentor"
-								placeholder="eg: John Doe"
-								value={values.mentor ?? ""}
-								variant="outlined"
-								label="Mentor email"
-								fullWidth
-								onChange={handleChange}
-								helperText={touched.mentor && errors.mentor && errors.mentor}
-								onBlur={handleBlur}
-								error={!!(touched.mentor && errors.mentor)}
-							/>
-						)}
-					</Grid>
-					<Grid item xs={12}>
-						{isLoading ? (
-							<Skeleton variant="text" />
-						) : (
-							<TextField
-								name="co_mentor"
-								placeholder="eg: John Doe"
-								value={values.co_mentor ?? ""}
-								variant="outlined"
-								label="Co-mentor email"
-								fullWidth
-								onChange={handleChange}
-								helperText={touched.co_mentor && errors.co_mentor && errors.co_mentor}
-								onBlur={handleBlur}
-								error={!!(touched.co_mentor && errors.co_mentor)}
-							/>
-						)}
-					</Grid>
-
-					<Typography variant="h6">Links</Typography>
-					<Grid item xs={12}>
-						{isLoading ? (
-							<Skeleton variant="text" />
-						) : (
-							<TextField
-								name="blog"
-								placeholder="eg: https://www.thearmchaircritic.org"
-								value={values.blog ?? ""}
-								variant="outlined"
-								label="Blog Link"
-								fullWidth
-								onChange={handleChange}
-								helperText={touched.blog && errors.blog && errors.blog}
-								onBlur={handleBlur}
-								error={!!(touched.blog && errors.blog)}
-							/>
-						)}
-					</Grid>
-					<Grid item xs={12}>
-						{isLoading ? (
-							<Skeleton variant="text" />
-						) : (
-							<TextField
-								name="gantt_chart"
-								placeholder="eg: https://docs.google.com/..."
-								value={values.gantt_chart ?? ""}
-								variant="outlined"
-								label="Link to Gantt Chart"
-								fullWidth
-								onChange={handleChange}
-								helperText={touched.gantt_chart && errors.gantt_chart && errors.gantt_chart}
-								onBlur={handleBlur}
-								error={!!(touched.gantt_chart && errors.gantt_chart)}
-							/>
-						)}
-					</Grid>
-					<Button type="submit">Submit</Button>
 				</Grid>
 			</form>
 		</>
