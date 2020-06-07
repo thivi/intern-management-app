856c51cb9002e4c867541fd82f26bb58c67fadf0
Thivi <theviyanthan@outlook.com>
2020-06-08 04:06:03 +0530

    Index themes

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,7 +1,21 @@
 import { makeStyles, Theme } from "@material-ui/core/styles";
-import { appBar, drawer, drawerPaper, appBarTitle, toolbar, root, content, paper } from "./app-layout";
-import { gridForm, gridRightMargin, pagination } from "./list-page";
-import { popOver } from "./popover";
+import { appBar, drawer, drawerPaper, appBarTitle, toolbar, root, content, paper, avatar } from "./app-layout";
+import {
+	gridForm,
+	gridRightMargin,
+	pagination,
+	addPaper,
+	addButtonGrid,
+	listHeader,
+	listPaper,
+	list,
+	sortButton,
+	search,
+	filterGrid,
+} from "./list-page";
+import { popOver, primaryButton, leftAlignedGrid } from "./generic";
+import { profileAvatar, fieldsHeader, fieldsPaper } from "./profile";
+import { tile, tileRow, tileGrid, tileColumn } from "./dashboard";
 
 const useStyles = makeStyles((theme: Theme) => ({
 	appBar: appBar(theme),
@@ -16,6 +30,25 @@ const useStyles = makeStyles((theme: Theme) => ({
 	gridRightMargin: gridRightMargin(theme),
 	pagination: pagination(theme),
 	popOver: popOver(theme),
+	avatar: avatar(theme),
+	profileAvatar: profileAvatar(theme),
+	fieldsHeader: fieldsHeader(theme),
+	fieldsPaper: fieldsPaper(theme),
+	primaryButton: primaryButton(theme),
+	leftAlignedGrid: leftAlignedGrid(theme),
+	addPaper: addPaper(theme),
+	addButtonGrid: addButtonGrid(theme),
+	listHeader: listHeader(theme),
+	listPaper: listPaper(theme),
+	list: list(theme),
+	sortButton: sortButton(theme),
+	search: search(theme),
+	filterGrid: filterGrid(theme),
+	tile: tile(theme),
+	tileRow: tileRow(theme),
+	tileGrid: tileGrid(theme),
+	tileColumn: tileColumn(theme),
 }));
 
 export default useStyles;
+export * from "./theme";
