ec6e74160e7c1031fa7a347664c9165f79898952
Thivi <theviyanthan@outlook.com>
2020-06-08 04:06:52 +0530

    Add profile theme

---
+++ src/theme/profile.ts
@@ -0,0 +1,15 @@
+import { Theme } from "@material-ui/core";
+
+export const profileAvatar = (theme: Theme) => ({
+	width: theme.spacing(20),
+	height: theme.spacing(20),
+});
+
+export const fieldsHeader = (theme: Theme) => ({
+	marginBottom: theme.spacing(4),
+});
+
+export const fieldsPaper = (theme: Theme) => ({
+    marginTop: theme.spacing(4),
+    padding: theme.spacing(3)
+});
