ec8992b6beaaf778d4f80f0cbe0958936a1b0aa3
Thivi <theviyanthan@outlook.com>
2020-06-09 02:44:23 +0530

    Add backButton class

--- src/theme/profile.ts
+++ src/theme/profile.ts
@@ -10,6 +10,10 @@ export const fieldsHeader = (theme: Theme) => ({
 });
 
 export const fieldsPaper = (theme: Theme) => ({
-    marginTop: theme.spacing(4),
-    padding: theme.spacing(3)
+	marginTop: theme.spacing(4),
+	padding: theme.spacing(3),
+});
+
+export const backButton = (theme: Theme) => ({
+	marginBottom: theme.spacing(1),
 });
