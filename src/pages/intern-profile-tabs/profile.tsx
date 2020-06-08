65c96e6a6220557c036e12c1c83cd585efb14bfe
Thivi <theviyanthan@outlook.com>
2020-06-09 02:44:58 +0530

    Style profile tab

--- src/pages/intern-profile-tabs/profile.tsx
+++ src/pages/intern-profile-tabs/profile.tsx
@@ -1,6 +1,7 @@
 import React, { ReactElement } from "react";
 import { Profile } from "../../models";
-import { Grid, Typography } from "@material-ui/core";
+import { Grid, Typography, Paper } from "@material-ui/core";
+import useStyles from "../../theme";
 
 interface ProfileTabPropsInterface {
 	profile: Profile;
@@ -8,61 +9,87 @@ interface ProfileTabPropsInterface {
 
 export const ProfileTab = (props: ProfileTabPropsInterface): ReactElement => {
 	const { profile } = props;
+
+	const classes = useStyles();
 	return (
-		<Grid container spacing={2}>
-			<Typography variant="h6">Personal Information</Typography>
-			<Grid item xs={12}>
-				<Typography>Name</Typography>
-				<Typography>{profile.Name}</Typography>
-			</Grid>
-			<Grid item xs={12}>
-				<Typography>Email</Typography>
-				<Typography>{profile.Email_ID}</Typography>
-			</Grid>
-			<Typography variant="h6">Academic Credentials</Typography>
-			<Grid item xs={12}>
-				<Typography>University</Typography>
-				<Typography>{profile.University}</Typography>
-			</Grid>
-			<Grid item xs={12}>
-				<Typography>Degree</Typography>
-				<Typography>{profile.Degree}</Typography>
-			</Grid>
+		<>
+			<Paper className={classes.fieldsPaper}>
+				<Grid container spacing={2}>
+					<Typography variant="h6">Personal Information</Typography>
+					<Grid item xs={12}>
+						<Typography variant="subtitle2">Name</Typography>
+						<Typography>{profile.Name}</Typography>
+					</Grid>
+					<Grid item xs={12}>
+						<Typography variant="subtitle2">Email</Typography>
+						<Typography>{profile.Email_ID}</Typography>
+					</Grid>
+				</Grid>
+			</Paper>
 
-			<Typography variant="h6">Internship Period</Typography>
-			<Grid item xs={12}>
-				<Typography>Joined Date</Typography>
-				<Typography>{profile.Joined_date}</Typography>
-			</Grid>
-			<Grid item xs={12}>
-				<Typography>Leaving Date</Typography>
-				<Typography>{profile.Leaving_date}</Typography>
-			</Grid>
+			<Paper className={classes.fieldsPaper}>
+				<Grid container spacing={2}>
+					<Typography variant="h6">Academic Credentials</Typography>
+					<Grid item xs={12}>
+						<Typography variant="subtitle2">University</Typography>
+						<Typography>{profile.University}</Typography>
+					</Grid>
+					<Grid item xs={12}>
+						<Typography variant="subtitle2">Degree</Typography>
+						<Typography>{profile.Degree}</Typography>
+					</Grid>
+				</Grid>
+			</Paper>
 
-			<Typography variant="h6">Contacts</Typography>
-			<Grid item xs={12}>
-				<Typography>Contact No.</Typography>
-				<Typography>{profile.Contact_no}</Typography>
-			</Grid>
+			<Paper className={classes.fieldsPaper}>
+				<Grid container spacing={2}>
+					<Typography variant="h6">Internship Period</Typography>
+					<Grid item xs={12}>
+						<Typography variant="subtitle2">Joined Date</Typography>
+						<Typography>{profile.Joined_date}</Typography>
+					</Grid>
+					<Grid item xs={12}>
+						<Typography variant="subtitle2">Leaving Date</Typography>
+						<Typography>{profile.Leaving_date}</Typography>
+					</Grid>
+				</Grid>
+			</Paper>
 
-			<Typography variant="h6">Mentors</Typography>
-			<Grid item xs={12}>
-				<Typography>Mentor</Typography>
-				<Typography>{profile.Mentor}</Typography>
-			</Grid>
-			<Grid item xs={12}>
-				<Typography>Co-mentor</Typography>
-				<Typography>{profile.Co_mentor}</Typography>
-			</Grid>
-			<Typography variant="h6">Links</Typography>
-			<Grid item xs={12}>
-				<Typography>Blog</Typography>
-				<Typography>{profile.Blog}</Typography>
-			</Grid>
-			<Grid item xs={12}>
-				<Typography>Gantt Chart</Typography>
-				<Typography>{profile.Gantt_chart}</Typography>
-			</Grid>
-		</Grid>
+			<Paper className={classes.fieldsPaper}>
+				<Grid container spacing={2}>
+					<Typography variant="h6">Contacts</Typography>
+					<Grid item xs={12}>
+						<Typography variant="subtitle2">Contact No.</Typography>
+						<Typography>{profile.Contact_no}</Typography>
+					</Grid>
+				</Grid>
+			</Paper>
+			<Paper className={classes.fieldsPaper}>
+				<Grid container spacing={2}>
+					<Typography variant="h6">Mentors</Typography>
+					<Grid item xs={12}>
+						<Typography variant="subtitle2">Mentor</Typography>
+						<Typography>{profile.Mentor}</Typography>
+					</Grid>
+					<Grid item xs={12}>
+						<Typography variant="subtitle2">Co-mentor</Typography>
+						<Typography>{profile.Co_mentor}</Typography>
+					</Grid>
+				</Grid>
+			</Paper>
+			<Paper className={classes.fieldsPaper}>
+				<Grid container spacing={2}>
+					<Typography variant="h6">Links</Typography>
+					<Grid item xs={12}>
+						<Typography variant="subtitle2">Blog</Typography>
+						<Typography>{profile.Blog}</Typography>
+					</Grid>
+					<Grid item xs={12}>
+						<Typography variant="subtitle2">Gantt Chart</Typography>
+						<Typography>{profile.Gantt_chart}</Typography>
+					</Grid>
+				</Grid>
+			</Paper>
+		</>
 	);
 };
