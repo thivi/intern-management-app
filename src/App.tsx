7cb50757c76f15d8e14e30404704e298fccf277f
Thivi <theviyanthan@outlook.com>
2020-06-12 04:26:47 +0530

    Add notification provider and component to the App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import { Authentication } from "./helpers/authentication";
+import { Authentication, Notification } from "./helpers";
 import { routes } from "./configs";
 import { ProtectedRoute } from "./helpers";
 import { RouteInterface } from "./models/routes";
-import { AppLayout } from "./layout";
+import { AppLayout, NotificationComponent } from "./layout";
 import { ThemeProvider } from "@material-ui/core";
 import { theme } from "./theme";
 
@@ -13,39 +13,42 @@ export const App = (): React.ReactElement => {
 		<BrowserRouter>
 			<Authentication>
 				<ThemeProvider theme={theme}>
-					<Switch>
-						{routes?.map((route: RouteInterface, index: number) => {
-							if (route.protected) {
-								return (
-									<ProtectedRoute
-										key={index}
-										path={route.path}
-										exact={route.exact}
-										component={route.component}
-										appLayout={route.appLayout}
-										permission={route.permission}
-									/>
-								);
-							} else {
-								return (
-									<Route
-										key={index}
-										path={route.path}
-										exact={route.exact}
-										component={
-											route.appLayout
-												? () => (
-														<AppLayout>
-															<route.component />
-														</AppLayout>
-												  )
-												: route.component
-										}
-									/>
-								);
-							}
-						})}
-					</Switch>
+					<Notification>
+						<NotificationComponent />
+						<Switch>
+							{routes?.map((route: RouteInterface, index: number) => {
+								if (route.protected) {
+									return (
+										<ProtectedRoute
+											key={index}
+											path={route.path}
+											exact={route.exact}
+											component={route.component}
+											appLayout={route.appLayout}
+											permission={route.permission}
+										/>
+									);
+								} else {
+									return (
+										<Route
+											key={index}
+											path={route.path}
+											exact={route.exact}
+											component={
+												route.appLayout
+													? () => (
+															<AppLayout>
+																<route.component />
+															</AppLayout>
+													  )
+													: route.component
+											}
+										/>
+									);
+								}
+							})}
+						</Switch>
+					</Notification>
 				</ThemeProvider>
 			</Authentication>
 		</BrowserRouter>
