c94fd2054e28514b1079cca30424b0ebc6ad1ee4
Thivi <theviyanthan@outlook.com>
2020-06-13 04:05:55 +0530

    Illustrate 404 component

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,8 +1,26 @@
 import React, { ReactElement, useContext } from "react";
 import { AuthContext } from "../helpers";
+import { Box, Typography } from "@material-ui/core";
+import { NotFoundGraphic } from "../theme/img";
 
 export const NotFound = (): ReactElement => {
 	const { authState } = useContext(AuthContext);
 
-	return authState?.authData?.role ? <h1>Page Not Found</h1> : null;
+	return authState?.authData?.role ? (
+		<Box display="flex" justifyContent="center">
+			<Box width={500}>
+				<Box display="flex" justifyContent="center">
+					<img src={NotFoundGraphic} alt="not-found" width={500} />
+				</Box>
+				<Box>
+					<Typography variant="h5" align="center">
+						The page you're looking for either doesn't exist or you don't have permission to access it.
+					</Typography>
+					<Typography variant="h6" color="textSecondary" align="center">
+						Contact your mentor to obtain the necessary permission.
+					</Typography>
+				</Box>
+			</Box>
+		</Box>
+	) : null;
 };
