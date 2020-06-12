c96ee7de8f0bd64d31d935c927b2ab29cca49fd7
Thivi <theviyanthan@outlook.com>
2020-06-13 03:19:04 +0530

    Illustrate dashboards

--- src/pages/dashboard/dashboard-intern.tsx
+++ src/pages/dashboard/dashboard-intern.tsx
@@ -37,7 +37,17 @@ import useStyles from "../../theme";
 import { findTimeOfTheDay, Notify } from "../../utils";
 import { Skeleton } from "@material-ui/lab";
 import { WorkOutlineOutlined } from "@material-ui/icons";
-import { MorningGraphic, NoonGraphic, EveningGraphic, NightGraphic } from "../../theme/img";
+import {
+	MorningGraphic,
+	NoonGraphic,
+	EveningGraphic,
+	NightGraphic,
+	BlogGraphic,
+	GitIssueGraphic,
+	ProjectGraphic,
+	PresentationGraphic,
+	PullRequestGraphic,
+} from "../../theme/img";
 
 export const DashboardIntern = (): ReactElement => {
 	const { authState } = useContext(AuthContext);
@@ -197,31 +207,33 @@ export const DashboardIntern = (): ReactElement => {
 			<Grid item xs={6}>
 				<Paper className={`${classes.tile} ${classes.centeredTile}`}>
 					{!isLoading ? (
-						<Chart
-							data={[
-								{
-									type: `Completed (${intern.projectTasksCompletion * 100}%)`,
-									value: intern?.projectTasksCompletion * 100,
-								},
-								{
-									type: `Incomplete (${100 - intern?.projectTasksCompletion * 100}%)`,
-									value: 100 - intern?.projectTasksCompletion * 100,
-								},
-							]}
-							width={500}
-						>
-							<PieSeries valueField="value" argumentField="type" innerRadius={0.6} />
-							<Title text="Project Tasks Completed" />
-							<Animation />
-							<Legend />
-							<EventTracker />
-							<Tooltip />
-						</Chart>
+						<Box padding={3}>
+							<Chart
+								data={[
+									{
+										type: `Completed (${intern.projectTasksCompletion * 100}%)`,
+										value: intern?.projectTasksCompletion * 100,
+									},
+									{
+										type: `Incomplete (${100 - intern?.projectTasksCompletion * 100}%)`,
+										value: 100 - intern?.projectTasksCompletion * 100,
+									},
+								]}
+								width={500}
+							>
+								<PieSeries valueField="value" argumentField="type" innerRadius={0.6} />
+								<Title text="Project Tasks Completed" />
+								<Animation />
+								<Legend />
+								<EventTracker />
+								<Tooltip />
+							</Chart>
+						</Box>
 					) : (
-						<>
+						<Box padding={3}>
 							<Skeleton variant="text" height={70} width={400} />
 							<Skeleton variant="circle" height={400} width={400} animation="wave" />
-						</>
+						</Box>
 					)}
 				</Paper>
 			</Grid>
@@ -229,150 +241,180 @@ export const DashboardIntern = (): ReactElement => {
 				<Grid container item xs={12} className={classes.tileRow}>
 					<Grid item xs={6} className={classes.tileGrid}>
 						<Paper className={classes.tile}>
-							{isLoading ? (
-								<div className={classes.centerAlign}>
-									<Skeleton variant="text" height={100} width={50} />
-								</div>
-							) : (
-								<Typography variant="h3" align="center">
-									{intern?.blogs}
+							<Box padding={3}>
+								<Box display="flex" justifyContent="center">
+									<img src={BlogGraphic} alt="intern" width={60} />
+								</Box>
+								{isLoading ? (
+									<div className={classes.centerAlign}>
+										<Skeleton variant="text" height={60} width={50} />
+									</div>
+								) : (
+									<Typography variant="h3" align="center">
+										{intern?.blogs}
+									</Typography>
+								)}
+								<Typography variant="h6" align="center" color="textSecondary">
+									Blogs Written
 								</Typography>
-							)}
-							<Typography variant="h6" align="center" color="textSecondary">
-								Blogs Written
-							</Typography>
-							<Button
-								onClick={() => {
-									history.push(BLOGS_PATH);
-								}}
-							>
-								More
-							</Button>
+								<Button
+									onClick={() => {
+										history.push(BLOGS_PATH);
+									}}
+									fullWidth
+								>
+									More
+								</Button>
+							</Box>
 						</Paper>
 					</Grid>
 					<Grid item xs={6} className={classes.tileGrid}>
 						<Paper className={classes.tile}>
-							{isLoading ? (
-								<div className={classes.centerAlign}>
-									<Skeleton variant="text" height={100} width={50} />
-								</div>
-							) : (
-								<Typography variant="h3" align="center">
-									{intern?.gitIssues}
+							<Box padding={3}>
+								<Box display="flex" justifyContent="center">
+									<img src={GitIssueGraphic} alt="intern" width={60} />
+								</Box>
+								{isLoading ? (
+									<div className={classes.centerAlign}>
+										<Skeleton variant="text" height={60} width={50} />
+									</div>
+								) : (
+									<Typography variant="h3" align="center">
+										{intern?.gitIssues}
+									</Typography>
+								)}
+								<Typography variant="h6" color="textSecondary" align="center">
+									Git Issues Raised
 								</Typography>
-							)}
-							<Typography variant="h6" color="textSecondary" align="center">
-								Git Issues Raised
-							</Typography>
-							<Button
-								onClick={() => {
-									history.push(GIT_ISSUES_PATH);
-								}}
-							>
-								More
-							</Button>
+								<Button
+									onClick={() => {
+										history.push(GIT_ISSUES_PATH);
+									}}
+									fullWidth
+								>
+									More
+								</Button>
+							</Box>
 						</Paper>
 					</Grid>
 				</Grid>
 				<Grid container item xs={12} className={classes.tileRow}>
 					<Grid item xs={6} className={classes.tileGrid}>
 						<Paper className={classes.tile}>
-							{isLoading ? (
-								<div className={classes.centerAlign}>
-									<Skeleton variant="text" height={100} width={50} />
-								</div>
-							) : (
-								<Typography variant="h3" align="center">
-									{intern?.pullRequests}
+							<Box padding={3}>
+								<Box display="flex" justifyContent="center">
+									<img src={PullRequestGraphic} alt="intern" width={80} />
+								</Box>
+								{isLoading ? (
+									<div className={classes.centerAlign}>
+										<Skeleton variant="text" height={60} width={50} />
+									</div>
+								) : (
+									<Typography variant="h3" align="center">
+										{intern?.pullRequests}
+									</Typography>
+								)}
+								<Typography variant="h6" align="center" color="textSecondary">
+									Pull Requests Raised
 								</Typography>
-							)}
-							<Typography variant="h6" align="center" color="textSecondary">
-								Pull Requests Raised
-							</Typography>
-							<Button
-								onClick={() => {
-									history.push(PULL_REQUESTS_PATH);
-								}}
-							>
-								More
-							</Button>
+								<Button
+									onClick={() => {
+										history.push(PULL_REQUESTS_PATH);
+									}}
+									fullWidth
+								>
+									More
+								</Button>
+							</Box>
 						</Paper>
 					</Grid>
 					<Grid item xs={6} className={classes.tileGrid}>
 						<Paper className={classes.tile}>
-							{isLoading ? (
-								<div className={classes.centerAlign}>
-									<Skeleton variant="text" height={100} width={50} />
-								</div>
-							) : (
-								<Typography variant="h3" align="center">
-									{intern?.presentationsOrWebinars}
+							<Box padding={3}>
+								<Box display="flex" justifyContent="center">
+									<img src={PresentationGraphic} alt="intern" width={60} />
+								</Box>
+								{isLoading ? (
+									<div className={classes.centerAlign}>
+										<Skeleton variant="text" height={60} width={50} />
+									</div>
+								) : (
+									<Typography variant="h3" align="center">
+										{intern?.presentationsOrWebinars}
+									</Typography>
+								)}
+								<Typography variant="h6" align="center" color="textSecondary">
+									Presentations/Webinars Done
 								</Typography>
-							)}
-							<Typography variant="h6" align="center" color="textSecondary">
-								Presentations/Webinars Done
-							</Typography>
-							<Button
-								onClick={() => {
-									history.push(PRESENTATIONS_OR_WEBINARS_PATH);
-								}}
-							>
-								More
-							</Button>
+								<Button
+									onClick={() => {
+										history.push(PRESENTATIONS_OR_WEBINARS_PATH);
+									}}
+									fullWidth
+								>
+									More
+								</Button>
+							</Box>
 						</Paper>
 					</Grid>
 				</Grid>
 			</Grid>
 			<Grid item xs={12}>
 				<Paper className={classes.tile}>
-					{isLoading ? (
-						<div className={classes.centerAlign}>
-							<Skeleton variant="text" height={100} width={50} />
-						</div>
-					) : (
-						<Typography variant="h3" align="center">
-							{intern?.projects instanceof Array && intern?.projects.length}
+					<Box padding={3}>
+						<Box display="flex" justifyContent="center">
+							<img src={ProjectGraphic} alt="intern" width={80} />
+						</Box>
+						{isLoading ? (
+							<div className={classes.centerAlign}>
+								<Skeleton variant="text" height={60} width={50} />
+							</div>
+						) : (
+							<Typography variant="h3" align="center">
+								{intern?.projects instanceof Array && intern?.projects.length}
+							</Typography>
+						)}
+						<Typography variant="h6" color="textSecondary" align="center">
+							Projects
 						</Typography>
-					)}
-					<Typography variant="h6" color="textSecondary" align="center">
-						Projects
-					</Typography>
-					<div>
-						<Typography variant="subtitle1">Latest: </Typography>
-						<List>
-							{isLoading ? (
-								<>
-									<Skeleton variant="text" height={50} />
-									<Skeleton variant="text" height={50} />
-									<Skeleton variant="text" height={50} />
-									<Skeleton variant="text" height={50} />
-									<Skeleton variant="text" height={50} />
-								</>
-							) : (
-								internInfo?.projects.map((project: Project, index: number) => {
-									if (index < 4) {
-										return (
-											<ListItem>
-												<ListItemIcon>
-													<WorkOutlineOutlined />
-												</ListItemIcon>
-												<Typography>{project.Title}</Typography>
-											</ListItem>
-										);
-									}
+						<div>
+							<Typography variant="subtitle1">Latest: </Typography>
+							<List>
+								{isLoading ? (
+									<>
+										<Skeleton variant="text" height={50} />
+										<Skeleton variant="text" height={50} />
+										<Skeleton variant="text" height={50} />
+										<Skeleton variant="text" height={50} />
+										<Skeleton variant="text" height={50} />
+									</>
+								) : (
+									internInfo?.projects.map((project: Project, index: number) => {
+										if (index < 4) {
+											return (
+												<ListItem>
+													<ListItemIcon>
+														<WorkOutlineOutlined />
+													</ListItemIcon>
+													<Typography>{project.Title}</Typography>
+												</ListItem>
+											);
+										}
 
-									return null;
-								})
-							)}
-						</List>
-					</div>
-					<Button
-						onClick={() => {
-							history.push(PROJECTS_PATH);
-						}}
-					>
-						More
-					</Button>
+										return null;
+									})
+								)}
+							</List>
+						</div>
+						<Button
+							onClick={() => {
+								history.push(PROJECTS_PATH);
+							}}
+							fullWidth
+						>
+							More
+						</Button>
+					</Box>
 				</Paper>
 			</Grid>
 		</Grid>
