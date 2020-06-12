c0e81394cc12cf21a521ddc199f7c26a5455e5fa
Thivi <theviyanthan@outlook.com>
2020-06-13 02:37:09 +0530

    Modify time of the day util

--- src/utils/time-of-the-day.ts
+++ src/utils/time-of-the-day.ts
@@ -1,8 +1,31 @@
-export const findTimeOfTheDay = () => {
+import { TimeOfTheDay } from "../models";
+
+export const findTimeOfTheDay = (): TimeOfTheDay => {
 	const hour = new Date().getHours();
 
-	if (hour > 18) return "Evening";
-	if (hour > 12) return "Afternoon";
+	if (hour > 19)
+		return {
+			text: "Evening",
+			image: "night",
+		};
+	if (hour > 16)
+		return {
+			text: "Evening",
+			image: "evening",
+		};
+	if (hour > 12)
+		return {
+			text: "Afternoon",
+			image: "noon",
+		};
+	if (hour > 6)
+		return {
+			text: "Morning",
+			image: "morning",
+		};
 
-	return "Morning";
+	return {
+		text: "Morning",
+		image: "night",
+	};
 };
