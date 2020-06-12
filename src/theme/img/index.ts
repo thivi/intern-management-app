5789f762ad31f3b09cea13069a8d391e0fa44b8e
Thivi <theviyanthan@outlook.com>
2020-06-13 02:37:41 +0530

    Index new images

--- src/theme/img/index.ts
+++ src/theme/img/index.ts
@@ -1,5 +1,13 @@
 import Box from "./box.svg";
 import Search from "./search.svg";
+import Morning from "./morning.svg";
+import Noon from "./sun.svg";
+import Evening from "./beach.svg";
+import Night from "./night.svg";
 
 export const BoxGraphic = Box;
 export const SearchGraphic = Search;
+export const MorningGraphic = Morning;
+export const NoonGraphic = Noon;
+export const EveningGraphic = Evening;
+export const NightGraphic = Night;
