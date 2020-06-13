c17478a88693b0e33e13a0e73048232066c7a3d0
Thivi <theviyanthan@outlook.com>
2020-06-14 00:21:12 +0530

    Add staging and prod sites to firebase

--- src/constants/api-endpoints.ts
+++ src/constants/api-endpoints.ts
@@ -1,6 +1,6 @@
-export const spreadSheetId = "1-uQ2vuWpxYfNaRkGfh9zaZ4Vmwql1wDh1olLczKLgzM";
+export const spreadSheetId = process.env.REACT_APP_SPREADSHEET_ID;
 
 export const apiEndpoints = {
-    sheet: `${spreadSheetId}/values`,
-    googleProfile:"https://www.googleapis.com/oauth2/v3/userinfo?alt=json"
+	sheet: `${spreadSheetId}/values`,
+	googleProfile: "https://www.googleapis.com/oauth2/v3/userinfo?alt=json",
 };
