34f155b74e9053f7a4fd63ebda439e68a684e891
Thivi <theviyanthan@outlook.com>
2020-06-15 04:31:21 +0530

    Add progress loader hook

---
+++ src/helpers/progress-loader-hook.tsx
@@ -0,0 +1,51 @@
+import { useState, useEffect } from "react";
+import { NetworkRequestMessage } from "../models";
+
+export const useProgressLoader = (): number => {
+	const [progress, setProgress] = useState<Map<string, boolean>>(new Map());
+	const [loading, setLoading] = useState(false);
+	const [percent, setPercent] = useState(0);
+
+	useEffect(() => {
+		if (loading && percent === 0) {
+			setPercent(1);
+		} else if (!loading && percent === 1) {
+			setPercent(100);
+			setTimeout(() => {
+				setPercent(0);
+			}, 1000);
+		}
+	}, [loading, percent]);
+
+	const resetProgress = () => {
+		setProgress(new Map());
+	};
+
+	useEffect(() => {
+		if (progress.size > 0) {
+			let isLoading = false;
+			progress.forEach((value: boolean) => {
+				if (value) {
+					isLoading = true;
+				}
+			});
+
+			if (!isLoading) {
+				setLoading(false);
+				resetProgress();
+			}
+		}
+	}, [progress]);
+
+	onmessage = ({ data }: { data: NetworkRequestMessage }) => {
+		if (data.type === "progress") {
+			!loading && data.loading && setLoading(true);
+
+			const temp = new Map(progress);
+			temp.set(data.id, data.loading);
+			setProgress(temp);
+		}
+	};
+
+	return percent;
+};
