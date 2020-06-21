0b2b9db3d870daf7ce6dc1c7da912977256ffee4
Thivi <theviyanthan@outlook.com>
2020-06-21 14:47:39 +0530

    Make drawer responsive

--- src/theme/app-layout.ts
+++ src/theme/app-layout.ts
@@ -1,41 +1,53 @@
 import { Theme } from "@material-ui/core";
-import { DRAWER_WIDTH } from "./constants";
+import { DRAWER_WIDTH, TOP_BAR_HEIGHT_SM_UP, TOP_BAR_HEIGHT_SM_DOWN } from "./constants";
+
+const toolbarHeight = (theme: Theme) => (theme.breakpoints.up("sm") ? TOP_BAR_HEIGHT_SM_UP : TOP_BAR_HEIGHT_SM_DOWN);
 
 export const appBar = (theme: Theme) => ({
-    zIndex: theme.zIndex.drawer + 1
+	zIndex: theme.zIndex.drawer + 1
 });
 
 export const appBarTitle = (theme: Theme) => ({
-    flexGrow: 1
+	flexGrow: 1
 });
 
 export const avatar = (theme: Theme) => ({
-    marginLeft: theme.spacing(1)
+	marginLeft: theme.spacing(1)
 });
 
 export const drawerPaper = (theme: Theme) => ({
-    width: DRAWER_WIDTH,
-    minHeight: "calc(100vh - "+theme.mixins.toolbar.minHeight+"px)",
-	position: "absolute" as const,
+	width: DRAWER_WIDTH,
+	minHeight: "calc(100vh - " + toolbarHeight(theme) + "px)",
+	position: "absolute" as const
 });
 
 export const drawer = (theme: Theme) => ({
-    width: DRAWER_WIDTH,
-    flexShrink: 0,
-    position: "relative" as const
+	width: DRAWER_WIDTH,
+	flexShrink: 0,
+	position: "relative" as const,
+	"& .MuiBackdrop-root": {
+		top: toolbarHeight(theme)
+	}
 });
 
 export const toolbar = (theme: Theme) => theme.mixins.toolbar;
 
 export const root = (theme: Theme) => ({
-    display: "flex"
+	display: "flex"
 });
 
 export const content = (theme: Theme) => ({
 	flexGrow: 1,
-	padding:theme.spacing(3)
+	padding: theme.spacing(3)
 });
 
 export const paper = (theme: Theme) => ({
 	padding: theme.spacing(3)
+});
+
+export const floatingMenu = (theme: Theme) => ({
+	top: toolbarHeight(theme)+"!important"
+});
+export const menuIcon = (theme: Theme) => ({
+	color: theme.palette.common.white
 });
