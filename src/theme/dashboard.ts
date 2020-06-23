75e7d21eaf552327bb7be6ac768a1749a0adef00
Thivi <theviyanthan@outlook.com>
2020-06-24 04:44:20 +0530

    Add donutchart class

--- src/theme/dashboard.ts
+++ src/theme/dashboard.ts
@@ -5,21 +5,25 @@ export const tile = (theme: Theme) => ({
 	height: "100%",
 	display: "flex",
 	flexDirection: "column" as const,
-	justifyContent: "space-between" as const,
+	justifyContent: "space-between" as const
 });
 
 export const centeredTile = (theme: Theme) => ({
-	alignItems: "center",
+	alignItems: "center"
 });
 
 export const tileRow = (theme: Theme) => ({
-	height: "50%",
+	height: "50%"
 });
 
 export const tileGrid = (theme: Theme) => ({
 	height: "100%",
 	paddingRight: theme.spacing(1),
-	paddingLeft: theme.spacing(1),
+	paddingLeft: theme.spacing(1)
 });
 
-export const tileColumn = (theme: Theme) => ({});
+export const donutChart = (theme: Theme) => ({
+	"& #center-container": {
+		flexDirection: "column!important"
+	}
+});
