61d5d023dbfdfebf3f66031e89ca34afe54f071e
Thivi <theviyanthan@outlook.com>
2020-06-24 11:23:02 +0530

    Fix active tab border exceeding paper

--- src/theme/generic.ts
+++ src/theme/generic.ts
@@ -1,7 +1,7 @@
 import { Theme } from "@material-ui/core";
 
 export const popOver = (theme: Theme) => ({
-	pointerEvents: "none" as const,
+	pointerEvents: "none" as const
 });
 
 export const primaryButton = (theme: Theme) => ({
@@ -9,28 +9,32 @@ export const primaryButton = (theme: Theme) => ({
 	paddingLeft: theme.spacing(3),
 	paddingTop: theme.spacing(1.75),
 	paddingBottom: theme.spacing(1.75),
-	fontSize: theme.spacing(2),
+	fontSize: theme.spacing(2)
 });
 
 export const leftAlignedGrid = (theme: Theme) => ({
 	marginTop: theme.spacing(4),
 	display: "flex",
-	justifyContent: "flex-end",
+	justifyContent: "flex-end"
 });
 
 export const centerAlign = (theme: Theme) => ({
 	display: "flex",
-	justifyContent: "center",
+	justifyContent: "center"
 });
 
 export const coloredBackground = (theme: Theme) => ({
-	backgroundColor: theme.palette.primary.main,
+	backgroundColor: theme.palette.primary.main
 });
 
 export const primaryTextOnColoredBackground = (theme: Theme) => ({
-	color: theme.palette.getContrastText(theme.palette.primary.main),
+	color: theme.palette.getContrastText(theme.palette.primary.main)
 });
 
 export const secondaryTextOnColoredBackground = (theme: Theme) => ({
-	color: theme.palette.getContrastText(theme.palette.secondary.light),
+	color: theme.palette.getContrastText(theme.palette.secondary.light)
+});
+
+export const overflowHidden = (theme: Theme) => ({
+	overflow: "hidden"
 });
